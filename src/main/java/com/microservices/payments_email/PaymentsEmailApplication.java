219c20a46965ecdd92f7855373cbff963b0716ef
victornext <victorbrs517@gmail.com>
2026-02-10 15:30:08 -0300

    Basic structure

---
+++ src/main/java/com/microservices/payments_email/PaymentsEmailApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.payments_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsEmailApplication.class, args);
+	}
+
+}
